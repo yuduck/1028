95b3389925154c649c7c9e1cebd1129f51e04eb2
user <user@2-4>
2016-10-28 10:10:35 +0800

    1028

---
+++ WebApplication2/Controllers/yuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class yuController : Controller
+    {
+        // GET: yu
+        public ActionResult yuIndex()
+        {
+            return View();
+        }
+    }
+}
